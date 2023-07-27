c9b41ec116ddf150e729f1e42ec82ad477eadeb6
tsutoy <tsutoy@users.noreply.github.com>
2023-07-27 23:33:21 +0000

    action:bump package versions

---
+++ apps/sidebar/scripts/bumpVersion.js
@@ -0,0 +1,54 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const github_1 = require("@actions/github");
+const shell_1 = require("./utils/shell");
+function bumpVersion(type) {
+    const { stdout } = shell_1.shell.exec(`yarn version --${type} --no-commit-hooks --no-git-tag-version`, { onErrorMessage: `failed to bump ${github_1.context} version.` });
+    const version = (stdout.match(/(?<=New version: )[0-9.]*/g) || [''])[0];
+    return version;
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const bumpType = 'patch';
+        core.info(`Version bump type: ${bumpType}`);
+        const version = bumpVersion(bumpType);
+        core.info(`Bump to version: ${version}`);
+        shell_1.shell.exec(`echo "version=${version}" >> $GITHUB_OUTPUT`, {
+            onErrorMessage: 'failed to output version.',
+        });
+    });
+}
+main();
