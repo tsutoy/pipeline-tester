e8a3a5888ebae2062c97dd3feaabeb370ef44653
tsutoy <140764473+tsutoy@users.noreply.github.com>
2023-07-31 04:37:27 +0000

    test

---
+++ scripts/bumpVersion.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core';
+import { shell } from './utils/shell';
+
+type BranchTypes = 'major' | 'minor' | 'patch';
+
+function bumpVersion(type: BranchTypes): string {
+  const { stdout } = shell.exec(
+    `yarn version --${type} --no-commit-hooks --no-git-tag-version`,
+    { onErrorMessage: `failed to bump package.json version.` },
+  );
+
+  const version = (stdout.match(/(?<=New version: )[0-9.]*/g) || [''])[0];
+
+  return version;
+}
+
+async function main() {
+  const bumpType = 'patch';
+  core.info(`Version bump type: ${bumpType}`);
+
+  const version = bumpVersion(bumpType);
+
+  core.info(`Bump to version: ${version}`);
+
+  shell.exec(`echo "version=${version}" >> $GITHUB_OUTPUT`, {
+    onErrorMessage: 'failed to output version.',
+  });
+}
+
+main();
---
+++ templates/release.md
@@ -0,0 +1,4 @@
+[Full Changelog](https://github.com/eteg/deploy-app-zendesk/compare/{{last_tag}}...{{current_tag}})
+
+**Frontend**: {{frontend_output}}
+**Backend**: {{backend_output}}
